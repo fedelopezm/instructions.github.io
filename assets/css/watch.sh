3151bbe218da57c9a9635c0d3293fb7cdb5a1ccc
fedelopezm <flopezm17@gmail.com>
2020-07-14 21:35:05 -0400

    se organizo todos los detalles

---
+++ assets/css/watch.sh
@@ -0,0 +1,29 @@
+echo "Es para el unico archivo sass"
+echo "ingrese al codigo de este archivo para personalizarlo, estara comentado un ejemplo"
+sass --watch index.sass:index.css
+
+
+
+
+#!/bin/bash
+# echo "SASS"
+
+# read -p "Que archivo desea editar: " NAME
+
+# read -p "En que carpeta esta: dashboard - accounts - ninguna escribir asi como esta: " CARPETA
+
+# if [[ $CARPETA == "dashboard" ]]; then
+	
+# 	sass --watch $NAME.sass:../cuponTours/static/css/dashboard/$NAME.css
+
+# 	elif [[ $CARPETA == "accounts" ]]; then
+
+# 		sass --watch $NAME.sass:../cuponTours/static/css/accounts/$NAME.css
+
+# 	elif [[ $CARPETA == "ninguna" ]]; then
+		
+# 		sass --watch $NAME.sass:../cuponTours/static/css/$NAME.css
+
+# fi
+
+
